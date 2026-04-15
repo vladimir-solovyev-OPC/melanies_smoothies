9621ecd5734c5db8a00aea4c477e1ebd07afb0bb
vladimir-solovyev-OPC <vladimir.solovyev@on-point.com>
2026-04-15 16:09:47 +0200

    Update streamlit_app.py
    dataframe added

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("[https://www.smoothiefroot.com/api/fruit/watermelon](https://www.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+sf_df =st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
