c9dca691ec57a7cacc1256d966ddcdd0f2f90c7c
vladimir-solovyev-OPC <vladimir.solovyev@on-point.com>
2026-04-15 16:00:11 +0200

    Update streamlit_app.py
    json extenetion added

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
 
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
