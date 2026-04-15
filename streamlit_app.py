24af4dbbefb026b0f05f2df45e0274b824bf6a99
vladimir-solovyev-OPC <vladimir.solovyev@on-point.com>
2026-04-15 16:11:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests  
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -28,6 +29,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("[https://www.smoothiefroot.com/api/fruit/watermelon](https://www.smoothiefroot.com/api/fruit/watermelon)")  
+        sf_df =st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
@@ -41,6 +45,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order, icon="✅")
 
-import requests  
-smoothiefroot_response = requests.get("[https://www.smoothiefroot.com/api/fruit/watermelon](https://www.smoothiefroot.com/api/fruit/watermelon)")  
-sf_df =st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
